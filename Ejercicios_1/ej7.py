f5a8bc2911779f3a9db0c6091dca62b2e71a8fe6
facugirardi <facugirardi22@gmail.com>
2023-03-14 11:52:58 -0300

    TERMINADO

--- Ejercicios_1/ej7.py
+++ Ejercicios_1/ej7.py
@@ -3,10 +3,12 @@ def step(nums):
     if len(num) % 2 != 0:
         return False
  
-    for i in range(0,len(num),2):
-        if not (int(num[i:i+2][0])+1==int(num[i:i+2][1]) or int(num[i:i+2][0])-1==int(num[i:i+2][1])):
+    for i in range(0, len(num)-1, 2):
+        digito1 = int(num[i])
+        digito2 = int(num[i+1])
+        if abs(digito1 - digito2) != 1:
             return False
- 
+    
     return True
  
  
