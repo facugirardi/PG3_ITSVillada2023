28a2d854e9f2c11309e532b15bb1ffa2a6520c76
facugirardi <facugirardi22@gmail.com>
2023-03-14 11:30:49 -0300

    ej4

--- Ejercicios_1/ej4.py
+++ Ejercicios_1/ej4.py
@@ -1,1 +1,11 @@
-test
+def ordenamientoBurbuja(lista):
+    for nump in range(len(lista)-1,0,-1):
+        for i in range(nump):
+            if lista[i]>lista[i+1]:
+                temp = lista[i]
+                lista[i] = lista[i+1]
+                lista[i+1] = temp
+
+lista = [54,26,93,17,77,31,44,55,20]
+ordenamientoBurbuja(lista)
+print(lista)
