14226f90a7c9497f1609e6a952a93a24006981ca
facugirardi <facugirardi22@gmail.com>
2023-05-19 08:48:23 -0300

    test

--- Ejercicios/2_Ejercicios de Clases/test.py
+++ Ejercicios/2_Ejercicios de Clases/test.py
@@ -3,5 +3,9 @@ import pytest
 from ej1 import Persona
 
 class TestPersona:
+    def prueba_obj(self):
+        persona = Persona('facu')
+        assert persona.nombre == 'facu'
+
     def test_prueba(self):
         assert 'hola' == 'hola'
