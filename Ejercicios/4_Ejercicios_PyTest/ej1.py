1e5a6805ec7f259545ba119bb46eec8b20ac3e35
facugirardi <facugirardi22@gmail.com>
2023-06-05 09:01:33 -0300

    ej1

--- Ejercicios/4_Ejercicios_PyTest/ej1.py
+++ Ejercicios/4_Ejercicios_PyTest/ej1.py
@@ -5,3 +5,29 @@ Tiene al menos 8 caracteres.
 Contiene al menos una letra mayúscula, una letra minúscula y un número.
 No contiene caracteres especiales. 
 """
+import re
+
+
+class Password:
+
+    def validate_password(password):
+        if len(password) < 8:
+            return False
+        if not re.search(r"[a-z]", password):
+            return False
+        if not re.search(r"[A-Z]", password):
+            return False
+        if not re.search(r"\d", password):
+            return False
+        if re.search(r"\W", password):
+            return False
+        return True
+
+class TestPassword():
+# Pruebas para validate_password
+    def test_validate_password_valid(self):
+        assert Password.validate_password("Abcdefg1") == True
+        assert Password.validate_password("Abc1") == False
+        assert Password.validate_password("abcdefg1") == False
+        assert Password.validate_password("Abcdefgh") == False
+        assert Password.validate_password("Abcdefg1!") == False
